70adc15f96b18cdb14e9b86eae872224df5fdc1c
badtime1407 <badtime1407@gmail.com>
2026-03-08 00:17:41 +0700

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ import { booking } from "./routes/booking"
 const app = new Hono()
 
 app.use("*", cors({
-  origin: ["http://localhost:5173", "https://fancy-moon-c6d2.patipan.workers.dev"],
+  origin: ['http://localhost:5173', 'https://pre-wedding.pages.dev'],
   allowMethods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowHeaders: ["Content-Type", "Authorization"]
 }))
